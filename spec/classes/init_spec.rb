be6b9bcd9f007479d1f067c43d68df75589c9202
Andy Newton <amn@ecs.soton.ac.uk>
2015-08-27 03:30:15 -0700

    working rspec tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,16 +1,9 @@
 require 'spec_helper'
+require 'facts/graphicscards'
 # Partly cribbed from http://unethicalblogger.com/2014/03/01/testing-custom-facts-with-rspec.html
 
-describe 'Facter::Util::Fact' do
-
-  # Load facts before each test
- # before :each do
- #   GraphicsCardFacts.add_facts
- # end
-
-  describe 'graphicscards' do
-    allow(Facter::Util::Resolution).to receive(:exec).with("lspci -vmnn").
-      and_return("Device:	00:00.0
+# Fake lspci output, taken from a virtualbox VM and adding an nvidia card
+device_list = "Device:      00:00.0
 Class:	Host bridge [0600]
 Vendor:	Intel Corporation [8086]
 Device:	440FX - 82441FX PMC [Natoma] [1237]
@@ -52,13 +45,118 @@ Vendor:	Intel Corporation [8086]
 Device:	82371AB/EB/MB PIIX4 ACPI [7113]
 Rev:	08
 
-")
-      Facter.fact(:graphicscards).value.should == [{}]
-  end
+Device:	01:00.0
+Class:	VGA compatible controller [0300]
+Vendor:	NVIDIA Corporation [10de]
+Device:	GF116 [GeForce GTX 550 Ti] [1244]
+SVendor:	eVga.com. Corp. [3842]
+SDevice:	Device [1557]
+Rev:    a1
+
+"
+
+describe 'graphicscards', :type => :fact do
+
+  # Clear out facter before and after testing
+  before {Facter.clear}
+  after {Facter.clear}
+
+  # Only works on linux, this is the only fact it reads
+  let(:facts) do {:kernel => 'linux'} end
+  subject(:fact) { Facter.fact(:graphicscards) }
+
+
+#  it "should flag existence of nvidia graphics cards" do
+#    #Facter::Util::Resolution.stubs(:exec).with("lspci -vmnn").returns('TOAST')#device_list)
+#    expect(Facter.fact(:has_intel_graphicscard).value).to eq(false)
+#    expect(Facter.fact(:has_amd_graphicscard).value).to eq(false)
+#    expect(Facter.fact(:has_nvidia_graphicscard).value).to eq(true)
+#  end
+
+  it "should return correct graphics card list" do
+
+    # Fake out the lspci output
+    Facter::Util::Resolution.stubs(:exec).with("lspci -vmnn").returns(device_list)
+    expect(Facter.fact(:graphicscards).value).to eq([
+      {"class"=>"VGA compatible controller",
+       "class_numeric"=>"0300",
+       "vendor"=>"InnoTek Systemberatung GmbH",
+       "vendor_numeric"=>"80ee",
+       "device"=>"VirtualBox Graphics Adapter",
+       "device_numeric"=>"beef",
+      },
+      {"class"=>"VGA compatible controller",
+       "class_numeric"=>"0300",
+       "vendor"=>"NVIDIA Corporation",
+       "vendor_numeric"=>"10de",
+       "device"=>"GF116 [GeForce GTX 550 Ti]",
+       "device_numeric"=>"1244",
+       "svendor"=>"eVga.com. Corp.",
+       "svendor_numeric"=>"3842",
+       "sdevice"=>"Device",
+       "sdevice_numeric"=>"1557",
+      },
+    ])
+  end 
+  #expect(subject.value).to eql("i'm in your puppet!")
+
+#      Facter.fact(:graphicscards).value.should == [{}]
+#  end
 
   # Clean up facter after each test
   after :each do
     Facter.clear
     Facter.clear_messages
+  end
+end
+
+describe 'has_intel_graphicscard', :type => :fact do
+
+  # Clear out facter before and after testing
+  before {Facter.clear}
+  after {Facter.clear}
+
+  # Only works on linux, this is the only fact it reads
+  let(:facts) do {:kernel => 'linux'} end
+  subject(:fact) { Facter.fact(:graphicscards) }
+
+
+  it "should not flag existence of intel graphics cards" do
+    Facter::Util::Resolution.stubs(:exec).with("lspci -vmnn").returns(device_list)
+    expect(Facter.fact(:has_intel_graphicscard).value).to eq(false)
+  end
+end
+
+describe 'has_nvidia_graphicscard', :type => :fact do
+
+  # Clear out facter before and after testing
+  before {Facter.clear}
+  after {Facter.clear}
+
+  # Only works on linux, this is the only fact it reads
+  let(:facts) do {:kernel => 'linux'} end
+  subject(:fact) { Facter.fact(:graphicscards) }
+
+
+  it "should flag existence of nvidia graphics cards" do
+    Facter::Util::Resolution.stubs(:exec).with("lspci -vmnn").returns(device_list)
+    expect(Facter.fact(:has_nvidia_graphicscard).value).to eq(true)
+  end
+end
+
+describe 'has_amd_graphicscard', :type => :fact do
+
+  # Clear out facter before and after testing
+  before {Facter.clear}
+  after {Facter.clear}
+
+  # Only works on linux, this is the only fact it reads
+  let(:facts) do {:kernel => 'linux'} end
+  subject(:fact) { Facter.fact(:graphicscards) }
+
+
+  it "should not flag existence of amd graphics cards" do
+    Facter::Util::Resolution.stubs(:exec).with("lspci -vmnn").returns(device_list)
+    expect(Facter.fact(:has_amd_graphicscard).value).to eq(false)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,25 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+
+RSpec.configure do |config|
+
+  config.module_path = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures', 'modules')
+  config.manifest_dir = File.join(File.dirname(File.expand_path(__FILE__)), 'fixtures', 'manifests')
+  config.environmentpath = spec_path = File.expand_path(File.join(Dir.pwd, 'spec'))
+
+  config.before :each do
+    # Ensure that we don't accidentally cache facts and environment between
+    # test cases.  This requires each example group to explicitly load the
+    # facts being exercised with something like
+    # Facter.collection.loader.load(:ipaddress)
+    Facter.clear
+    Facter.clear_messages
+
+    RSpec::Mocks.setup
+  end
+
+  config.after :each do
+    RSpec::Mocks.verify
+    RSpec::Mocks.teardown
+  end
+
+end
